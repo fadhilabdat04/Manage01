9b47d75e6821de523c84688f8360851473a4b06f
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-07-11 01:18:33 +0700

    Update flood.py

--- Sriasih/modules/flood.py
+++ Sriasih/modules/flood.py
@@ -22,6 +22,7 @@ __HELP__ = """
 Anti-Flood system, the one who sends more than 10 messages in a row, gets muted for an hour (Except for admins).
 
 /flood [ENABLE|DISABLE] - Turn flood detection on or off
+ᴧꝛᴧʙ ꝛᴏʙᴏᴛ | @SiArab_Store ™
 """
 
 
