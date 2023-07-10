2fcbd8534390cd19aa19b1c90fb27a3c0abb6686
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-07-11 01:22:37 +0700

    Update music.py

--- Sriasih/modules/music.py
+++ Sriasih/modules/music.py
@@ -20,6 +20,7 @@ __HELP__ = """
 /ytmusic [link] To Download Music From Various Websites Including Youtube. [SUDOERS]
 /saavn [query] To Download Music From Saavn.
 /lyrics [query] To Get Lyrics Of A Song.
+ᴧꝛᴧʙ ꝛᴏʙᴏᴛ | @SiArab_Store ™
 """
 
 is_downloading = False
